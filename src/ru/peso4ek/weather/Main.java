8ebb1d6b64fe7d74a84db3c8a9e93b2af7a207c5
KissLyakov <kislyakovpav@yandex.ru>
2022-10-26 19:17:09 +0300

    Наброски приложения

--- src/ru/peso4ek/weather/Main.java
+++ src/ru/peso4ek/weather/Main.java
@@ -1,13 +1,20 @@
 package ru.peso4ek.weather;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
 import ru.peso4ek.weather.utils.WebParser;
 
 import java.io.IOException;
 
-public class Main {
+public class Main extends Application {
 
     public static void main(String[] args) throws IOException {
         WebParser webParser = new WebParser();
         webParser.getCities();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
     }
 }
